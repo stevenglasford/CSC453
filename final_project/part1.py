3fcd5054ff75322dc7c6b67b456835937cef63c5
stevenglasford <stevenglasford@gmail.com>
2020-05-01 18:26:17 -0500

    Combine all parts of the final project

--- final_project/part1.py
+++ final_project/part1.py
@@ -6,31 +6,28 @@ import numpy as np
 
 mat_contents = sio.loadmat("Homo_sapiens.mat")
 dense = mat_contents['network'].todense()
+G = nx.from_numpy_matrix(dense)
 numberOfNodes = np.shape(dense)[0]
 numberOfEdges = np.sum(np.tril(dense))
-averageDegree = (np.sum(dense)+np.trace(dense))/np.shape(dense)[0]
+averageDegree = (np.sum(dense) + np.trace(dense)) / np.shape(dense)[0]
 numberOfSelfLoops = nx.number_of_selfloops(G)
-#print(dense)
+# print(dense)
 
-#G=nx.read_edgelist(dense)
-G = nx.from_numpy_matrix(dense)
+# G=nx.read_edgelist(dense)
 
 
 print(
-    "Data from NetworkX\n"+
+    "Part 1 of Final Project\n"+
+    "Data from NetworkX\n" +
     nx.info(G) +
-    
-    "\n\nData from calculations from the adjacency list"+
-    "\nIs the graph directed: "  + str(nx.is_directed(G)) +
+
+    "\n\nData from calculations from the adjacency list" +
+    "\nIs the graph directed: " + str(nx.is_directed(G)) +
     "\nNumber of nodes: " + str(numberOfNodes) +
     "\nNumber of edges: " + str(numberOfEdges) +
     "\nNumber of self loops: " + str(numberOfSelfLoops) +
     "\nAverage degree: " + str(averageDegree) +
     "\n\nThe information from NetworkX is the same as the \n" +
     "calulations on the adjacency matrix, so we can have a high\n" +
-    "level of confidence that our network is correct." 
+    "level of confidence that our network is correct."
 )
-
-
-
-
---
+++ final_project/part2.py
@@ -0,0 +1,3 @@
+import part1
+
+print("\nPart 2 of the final project for CSC453")
